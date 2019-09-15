66a644a2488c11e6bc6c746b1d1410d29727cbe7
Lyon-Z <51785436+Lyon-Z@users.noreply.github.com>
2019-09-15 11:12:34 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var conh = document.getElementById('app');
+	var btn = document.getElementsByTagName('button')[0];
+	btn.onclick = function(){
+		conh.innerHTML = '郑璋亮'
+	}
+}
